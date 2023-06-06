025c78e888c92664ae6215ca6ad9cedc817e8897
Melih Özdemir <canmelihozdemir@gmail.com>
2023-06-06 10:22:14 +0300

    +added: CategoriesController API in Catalog

---
+++ MyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using FreeCourse.Services.Catalog.DTOs;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet] public async Task<IActionResult> GetAllAsync() => CreateActionResultInstance(await _categoryService.GetAllAsync());
+
+        [HttpGet("{id}")] public async Task<IActionResult> GetByIdAsync(string id) => CreateActionResultInstance(await _categoryService.GetByIdAsync(id));
+
+        [HttpPost] public async Task<IActionResult> CreateAsync(CategoryDto request) => CreateActionResultInstance(await _categoryService.CreateAsync(request));
+
+    }
+}
