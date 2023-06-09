03b1901d2ec49f22702c9eff102496e438cb44da
Melih Özdemir <canmelihozdemir@gmail.com>
2023-06-09 15:01:33 +0300

    +refactored: Authorization Configurations

--- MyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ MyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,5 +1,7 @@
 using FreeCourse.Services.Catalog.Services;
 using FreeCourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 //
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerURL"];
+    opt.Audience = "resource_catalog";
+    opt.RequireHttpsMetadata = false;
+});
+
+
 
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
@@ -14,12 +24,13 @@ builder.Services.AddScoped<ICourseService, CourseService>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
-//
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
 
-//
 
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
 builder.Services.AddSingleton<IDatabaseSettings>(sp=>
@@ -34,6 +45,9 @@ builder.Services.AddSingleton<IDatabaseSettings>(sp=>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -41,6 +55,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+//
+app.UseAuthentication();
+//
+
 
 app.UseAuthorization();
 
