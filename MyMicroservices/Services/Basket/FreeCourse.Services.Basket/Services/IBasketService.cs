cc6bb063c37ab8439c35574138a43310620e6a5b
Melih Özdemir <canmelihozdemir@gmail.com>
2023-06-14 11:55:11 +0300

    +added: IBasketService in Basket API

---
+++ MyMicroservices/Services/Basket/FreeCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Services.Basket.DTOs;
+using FreeCourse.Shared.DTOs;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasketAsync(string userId);
+        Task<Response<bool>> SaveOrUpdateAsync(BasketDto basketDto);
+        Task<Response<bool>> DeleteAsync(string userId);
+    }
+}
